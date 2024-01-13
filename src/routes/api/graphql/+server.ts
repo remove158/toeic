f31eb558cab0846a3ddab54670916c937b8e4f7b
Piyaphat Pinyo <osm@192.168.2.50>
2024-01-13 09:54:13 +0700

    feat: backend graphql

--- src/routes/api/graphql/+server.ts
+++ src/routes/api/graphql/+server.ts
@@ -1,24 +1,66 @@
-import { createSchema, createYoga } from 'graphql-yoga'
-import type { RequestEvent } from '@sveltejs/kit'
- 
+import { createSchema, createYoga } from 'graphql-yoga';
+import type { RequestEvent } from '@sveltejs/kit';
+import  { dictionary }  from './dictionary.js';
+import { getRandomInt, pickFourRandomly } from './utils.js';
+
 const yogaApp = createYoga<RequestEvent>({
-  schema: createSchema({
-    typeDefs: `
+	schema: createSchema({
+		typeDefs: `
 			type Query {
-				hello: String
+				question: Question
+				dictionary: [Dictionary]
 			}
+			
+			type Question {
+				word: String
+				choices: [String]
+				answer: String
+				meaning: String
+				POS: String
+				example: String
+				synonym: String
+			}
+
+			type Dictionary {
+				word: String
+				meaning: String
+				synonym: String
+				example: String
+			}
+
 		`,
-    resolvers: {
-      Query: {
-        hello: () => 'SvelteKit - GraphQL Yoga'
-      }
-    }
-  }),
-  // Needed to be defined explicitly because our endpoint lives at a different path other than `/graphql`
-  graphqlEndpoint: '/api/graphql',
- 
-  // Needed to let Yoga use sveltekit's Response object
-  fetchAPI: globalThis
+		resolvers: {
+			Query: {
+				question: () => {
+					const rnd = getRandomInt(dictionary.length);
+
+					const questionType = ['meaning', 'pos', 'synonym'];
+					const vocab = dictionary[rnd];
+					const randType = questionType[getRandomInt(3)];
+					const choices: string[] =
+						randType === 'pos'
+							? ['n.', 'v.', 'adv.', 'adj.'].sort(() => Math.random() - 0.5)
+							: pickFourRandomly(dictionary, rnd).map((item: any) => item[randType]);
+					const answer = (vocab as any)[randType];
+
+					return {
+						word: vocab.word,
+						meaning: vocab.meaning,
+						choices,
+						answer,
+						POS: vocab.pos,
+						example: vocab.example,
+						synonym: vocab.synonym
+					};
+				},
+				dictionary: () => dictionary
+			}
+		}
+	}),
+	// Needed to be defined explicitly because our endpoint lives at a different path other than `/graphql`
+	graphqlEndpoint: '/api/graphql',
+
+	// Needed to let Yoga use sveltekit's Response object
+	fetchAPI: globalThis
 })
- 
-export { yogaApp as GET, yogaApp as POST }
+export { yogaApp as GET, yogaApp as POST };
---
+++ src/routes/api/graphql/utils.ts
@@ -0,0 +1,19 @@
+export function getRandomInt(max: number) {
+	return Math.floor(Math.random() * max);
+}
+
+export function pickFourRandomly<T extends { pos: string, synonym: string }>(list: T[], index: number): Array<T> {
+	// Make a copy of the list
+	let copyList = [...list]
+	// Remove the index passed in from the list
+	copyList.splice(index, 1);
+	copyList = copyList.filter((item) => item?.pos !== "-" && item?.synonym !== "-");
+	// Shuffle the list randomly
+	const shuffledList = copyList.sort(() => Math.random() - 0.5);
+	// Pick the first 3 elements from the shuffled list
+	const randomThree = shuffledList.slice(0, 3);
+	// Add the index back to the list and shuffle again
+	const listWithIndex = [...randomThree, list[index]].sort(() => Math.random() - 0.5);
+	// Return the final list of 4 elements
+	return listWithIndex;
+}
