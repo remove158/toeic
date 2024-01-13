602484adffc5938db6638a437921df36b8f8ab80
Piyaphat Pinyo <osm@192.168.2.50>
2024-01-13 10:00:21 +0700

    init: tailwind css

---
+++ src/routes/components/header/component.svelte
@@ -0,0 +1,17 @@
+<div class="flex justify-between text-white items-center">
+	<h1 class="text-2xl">TOEIC Vocab</h1>
+	<div class="h-8 w-8 flex items-center justify-center">
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			fill="#fff"
+			class="bi bi-book-fill hover:cursor-pointer"
+			viewBox="0 0 16 16"
+		>
+			<path
+				d="M8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"
+			/>{' '}
+		</svg>
+	</div>
+</div>
