e6a0f14d56302d115cf6d62b9c9e3cca0472ba10
remove158 <remove158@gmail.com>
2024-01-25 07:05:16 +0700

    fix: title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 	import Header from '../components/header/component.svelte';
 </script>
 
-<title>TOEIC | Piyaphat.com</title>
+<title>TOEIC Vocab | Piyaphat.com</title>
 <Header />
 <div class="">
 	<slot />
